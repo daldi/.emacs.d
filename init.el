efa57aaeba48137ad4e2b46b9182ec1a5c52d860
Daniel Son <daniel.son@knewton.com>
2014-05-14 20:44:02 -0400

    Add flymake+flake8+pep8+pyflakes
    Adding and intializing git submodules for flake8 integration.

--- init.el
+++ init.el
@@ -4,12 +4,17 @@
 
 ;; All my emacs libraries will be under ~/.emacs.d
 (add-to-list 'load-path "~/.emacs.d")
-;Add all top-level subdirectories of .emacs.d to the load path
+;; Add all top-level subdirectories of .emacs.d to the load path
 (progn (cd "~/.emacs.d")
        (normal-top-level-add-subdirs-to-load-path))
 
+;; See column numbers
+(setq column-number-mode t)
+
 ;; Are we on a mac?
 (setq is-mac (equal system-type 'darwin))
+
+;; mouse stuff
 
 ;; Pbcopy, stdin->osx clipboard buffer tool
 (require 'pbcopy)
@@ -19,3 +24,11 @@
 ;; https://github.com/glasserc/ethan-wspace
 (require 'ethan-wspace)
 (global-ethan-wspace-mode 1)
+
+;; flymake-python-pyflakes
+;; adding flake8/pep8/pythonlint support to emacs
+(require 'flymake-easy)
+(require 'flymake-python-pyflakes)
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+(setq flymake-python-pyflakes-executable "flake8")
+(setq flymake-python-pyflakes-extra-arguments '("--max-line-length=100"))
